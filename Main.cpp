ddee81771052cc542a56b89874aed4b3d047d16b
divyaPrakash <divs4debu@gmail.com>
2017-02-01 00:01:11 +0530

    Added tupled and Refactored

--- Main.cpp
+++ Main.cpp
@@ -1,32 +1,49 @@
  
 #include <iostream>
+#include <tuple>
 #include <fstream>
 #include <unordered_set>
+#define LETTERS = "abcdefghijklmnopqrstuvwxyz"
 using namespace std;
+
+void  create_dictionary(unordered_set<string>&  set, ifstream& file){
+    string line;
+    try{
+        file.open("words.txt");
+    }
+    catch(ifstream::failure e){
+        cerr<<"Exception opening the file"<<endl;
+    }
+    while(getline(file, line))
+        set.insert(line);
+    file.close();
+}
+
+void print(const unordered_set<string>& set){
+    for (auto&x : set){
+        cout<<x<<endl;
+    }
+}
+
+void edits1(string word){
+    int size = word.length();
+    tuple<string,string> split [size+1];
+    for (int i =0 ; i< size+1;i++){
+        split[i] = make_tuple(word.substr(0,i), word.substr(i,size));
+    }
+    for (int i=0; i< size;i++){
+        cout<<get<0>(split[i])<<"\t"<<get<1>(split[i])<<endl;
+    }
+
+}
+
 
 int main(){
     ifstream myfile;
     unordered_set<string> myset;
-    long int count = 0;
-    string line;
-    myfile.open("words.txt");
-    if(myfile.is_open()){
-        while(getline(myfile,line)){
-            //cout<<line<<endl;
-            myset.insert(line);
-            count++;
-        }
-        cout<<"Words present are : "<<count<<endl;
-        myfile.close();
-    }
+    create_dictionary(myset, myfile);    
+    //print(myset);
+    edits1("hello");
     
-
-    
-    for (auto&x : myset){
-        cout<<x<<endl;
-    }
-    
-    cout<<myset.size()<<endl;
-
     return 0;
 }
